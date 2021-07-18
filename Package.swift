e9cf89f23efdd272dc8076ad3b259a041e10102c
Nick Partridge <nkpart@gmail.com>
2021-07-18 11:16:55 +1000

    Test update platforms to work around Xcode Beta 3 problems

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
   name: "combine-schedulers",
   platforms: [
-    .iOS(.v10),
-    .macOS(.v10_12),
+    .iOS(.v12),
+    .macOS(.v11),
     .tvOS(.v10),
     .watchOS(.v3),
   ],
